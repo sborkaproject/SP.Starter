b342a48c1303dcbfd23ac6c4320135ae41c435e4
Andrey Nikonov <a.einomi@gmail.com>
2018-02-27 16:47:38 +0300

    Change JS structure

--- src/media/js/index.js
+++ src/media/js/index.js
@@ -1,6 +1,29 @@
 global.$ = global.jQuery = require('jquery');
 global.TweenMax = require('TweenMax');
-require('./App');
+require('./utils/jqExtensions');
+
+const App = new (function App() {
+	this.env = require('./utils/ENV');
+	this.dom = require('./utils/DOM');
+	this.utils = require('./utils/Utils');
+
+	this.classes = {
+		Callback: require('./classes/Callback'),
+	};
+
+	this.helpers = {
+		SVGSprites: require('./helpers/SVGSprites'),
+	};
+
+	// Startup
+	$(() => {
+		// Remove _loading modificator
+		this.dom.$html.removeClass('_loading');
+	});
+})();
+
+// App → ProjectName
+global.ProjectName = global.App, delete global.App;
 
 if (module.hot) {
 	module.hot.accept();
