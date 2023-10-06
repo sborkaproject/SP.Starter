a1ffc29f1f6c0e7e1cef361d0e1f07f1d6a28fbb
Maksim Kalinin <kalinin.m.a@bk.ru>
2023-10-06 16:05:35 +0300

    Add robots flag

--- gulp-tasks/robots.js
+++ gulp-tasks/robots.js
@@ -1,6 +1,11 @@
 import gulp from 'gulp';
+import gulpif from 'gulp-if';
 import PATHS from '../paths';
 
+// npm run build -- --robots
 export default function robots() {
-	return gulp.src(PATHS.src.robots).pipe(gulp.dest(PATHS.build.html));
+	const args = process.argv.slice(2);
+	const isRobots = args.includes('--robots');
+
+	return gulp.src(PATHS.src.robots).pipe(gulpif(isRobots, gulp.dest(PATHS.build.html)));
 }
